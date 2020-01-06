79ab9f51aaa6b005f7df987dfcc20046e380edc2
Utah Kate Newman <utah.kate.newman@gmail.com>
2020-01-05 18:15:02 -0800

    commits tests

--- src/app/commits/commits.component.spec.ts
+++ src/app/commits/commits.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
 
+import { ResultsComponent } from '../results/results.component';
+import { CardComponent } from '../card/card.component';
 import { CommitsComponent } from './commits.component';
 
 describe('CommitsComponent', () => {
   let component: CommitsComponent;
   let fixture: ComponentFixture<CommitsComponent>;
 
+  const mockCommitsData = [
+    {
+      "commit": {
+        "author":
+        {
+          "name": "All of us",
+          "date": "2019-09-28 05:33:47"
+        },
+        "message": "omg what have I done"
+      }
+    }
+  ];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CommitsComponent ]
+      declarations: [
+        CommitsComponent,
+        MockComponent(CardComponent),
+        MockComponent(ResultsComponent)
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +36,22 @@ describe('CommitsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommitsComponent);
     component = fixture.componentInstance;
+    component.commit = mockCommitsData[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the correct commit information', () => {
+    let ele = fixture.nativeElement
+    let authorEle = ele.querySelector('.commit--author');
+    let dateEle = ele.querySelector('.commit--date');
+    let messageEle = ele.querySelector('.commit--message');
+
+    expect(authorEle.innerText).toContain(mockCommitsData[0].commit.author.name)
+    expect(dateEle.innerText).toContain(mockCommitsData[0].commit.author.date)
+    expect(messageEle.innerText).toContain(mockCommitsData[0].commit.message)
+  })
 });
